dde232ec9907773083bee7b46bd2945a4821eb60
Ryan Barrett <git@ryanb.org>
2013-01-24 16:45:42 -0800

    consolidate into models.py

---
+++ models.py
@@ -0,0 +1,192 @@
+"""Model base classes.
+"""
+
+__author__ = ['Ryan Barrett <freedom@ryanb.org>']
+
+import datetime
+import itertools
+import json
+import logging
+import urlparse
+
+import appengine_config
+from webutil import util
+from webutil import webapp2
+
+from google.appengine.api import taskqueue
+from google.appengine.ext import db
+
+
+class Source(util.KeyNameModel):
+  """A source to read posts from, e.g. a Facebook profile.
+
+  Each concrete source type should subclass this.
+  """
+
+  # POLL_TASK_DATETIME_FORMAT = '%Y-%m-%d-%H-%M-%S'
+  EPOCH = datetime.datetime.utcfromtimestamp(0)
+
+  # last_polled = db.DateTimeProperty(default=EPOCH)
+
+  # human-readable name for this source type. subclasses should override.
+  TYPE_NAME = None
+
+  url = db.LinkProperty()
+  picture = db.LinkProperty()
+
+  @classmethod
+  def create_new(cls, handler, **kwargs):
+    """Creates and saves a new Source and adds a poll task for it.
+
+    Args:
+      handler: the current webapp.RequestHandler
+      **kwargs: passed to new()
+    """
+    new = cls.new(handler, **kwargs)
+    new.save()
+    new.add_scan_task()
+    return new
+
+  @classmethod
+  def new(cls, handler, **kwargs):
+    """Factory method. Creates and returns a new instance for the current user.
+
+    To be implemented by subclasses.
+
+    Args:
+      handler: the current webapp.RequestHandler
+      **kwargs: passed to new()
+    """
+    raise NotImplementedError()
+
+  def display_name(self):
+    """Returns a human-readable name for this source, e.g. 'My Thoughts'.
+
+    To be implemented by subclasses.
+    """
+    raise NotImplementedError()
+
+  def type_display_name(self):
+    """Returns a human-readable name for this type of source, e.g. 'Facebook'.
+
+    To be implemented by subclasses.
+    """
+    raise NotImplementedError()
+
+  def get_freedom(self):
+    """Returns a list of Freedom template var dicts for posts and their comments.
+
+    To be implemented by subclasses.
+    """
+    raise NotImplementedError()
+
+  def add_scan_task(self, **kwargs):
+    """Adds a scan task for this source."""
+    taskqueue.add(queue_name='scan',
+                  params={'source_key': str(self.key())},
+                  **kwargs)
+
+
+class Destination(util.KeyNameModel):
+  """A web site to propagate posts to, e.g. a WordPress blog.
+
+  Each concrete destination class should subclass this class.
+  """
+
+  last_updated = db.DateTimeProperty()
+
+  def add_comment(self, comment):
+    """Posts the given comment to this site.
+
+    To be implemented by subclasses.
+
+    Args:
+      comment: Comment
+    """
+    raise NotImplementedError()
+
+
+class Post(util.KeyNameModel):
+  """A post to be propagated to a single destination.
+
+  Key name is 'POST_ID DEST KEY_NAME', e.g.
+  '123_456_789 Wordpress http://snarfed.org/w/_0'. The post id, destination
+  kind, and destination key name must not have spaces.
+
+  I could use the two serialized keys instead, but this makes manual inspection
+  and debugging easier.
+  """
+
+  STATUSES = ('new', 'processing', 'complete')
+
+  status = db.StringProperty(choices=STATUSES, default='new')
+  leased_until = db.DateTimeProperty()
+  # JSON data for this post from the source social network's API.
+  data = db.StringProperty()
+
+  @db.transactional
+  def get_or_save(self):
+    existing = db.get(self.key())
+    if existing:
+      logging.debug('Deferring to existing post %s.', existing.key().name())
+      return existing
+
+    logging.debug('New post to propagate: %s' % self.key().name())
+    taskqueue.add(queue_name='propagate', params={'post_key': str(self.key())})
+    self.save()
+    return self
+
+  @staticmethod
+  def make_key_name(id, dest):
+    """Returns the key name to use for a given post id and destination.
+
+    Args:
+      id: string, post id
+      dest: Destination instance
+    """
+    parts = (id, dest.kind(), dest.key_name())
+    for part in parts:
+      assert ' ' not in part
+    key_name = '%s %s %s' % parts
+
+  def send_slap(self):
+    util.urlfetch(post.endpoint, method='POST', payload=self.envelope(),
+                  headers={'Content-Type': 'application/magic-envelope+xml'})
+
+
+class Comment(util.KeyNameModel):
+  """A comment to be propagated.
+  """
+  STATUSES = ('new', 'processing', 'complete')
+
+  source = db.ReferenceProperty(reference_class=Source, required=True)
+  dest = db.ReferenceProperty(reference_class=Destination, required=True)
+  source_post_url = db.LinkProperty()
+  source_comment_url = db.LinkProperty()
+  dest_post_url = db.LinkProperty()
+  dest_comment_url = db.LinkProperty()
+  created = db.DateTimeProperty()
+  author_name = db.StringProperty()
+  author_url = db.LinkProperty()
+  content = db.TextProperty()
+
+  status = db.StringProperty(choices=STATUSES, default='new')
+  leased_until = db.DateTimeProperty()
+
+  @db.transactional
+  def get_or_save(self):
+    existing = db.get(self.key())
+    if existing:
+      logging.debug('Deferring to existing comment %s.', existing.key().name())
+      # this might be a nice sanity check, but we'd need to hard code certain
+      # properties (e.g. content) so others (e.g. status) aren't checked.
+      # for prop in self.properties().values():
+      #   new = prop.get_value_for_datastore(self)
+      #   existing = prop.get_value_for_datastore(existing)
+      #   assert new == existing, '%s: new %s, existing %s' % (prop, new, existing)
+      return existing
+
+    logging.debug('New comment to propagate: %s' % self.key().name())
+    taskqueue.add(queue_name='propagate', params={'comment_key': str(self.key())})
+    self.save()
+    return self
---
+++ models_test.py
@@ -0,0 +1,127 @@
+"""Unit tests for models.py.
+"""
+
+__author__ = ['Ryan Barrett <freedom@ryanb.org>']
+
+import json
+import mox
+
+import appengine_config
+from post import Post
+from webutil import testutil
+
+
+POST_VARS = {
+  'id': 'tag:facebook.com,2012:10102828452385634_39170557',
+  'author_name': 'Ryan Barrett',
+  'author_uri': 'acct:212038@facebook-webfinger.appspot.com',
+  # TODO: this should be the original domain link
+  'in_reply_to': 'tag:facebook.com,2012:10102828452385634',
+  'content': 'moire patterns: the new look for spring.',
+  'title': 'moire patterns: the new look for spring.',
+  'updated': '2012-05-21T02:25:25+0000',
+  }
+
+
+class SourceTest(testutil.HandlerTest):
+
+  def _test_create_new(self):
+    FakeSource.create_new(self.handler)
+    self.assertEqual(1, FakeSource.all().count())
+
+    tasks = self.taskqueue_stub.GetTasks('poll')
+    self.assertEqual(1, len(tasks))
+    source = FakeSource.all().get()
+    self.assertEqual('/_ah/queue/poll', tasks[0]['url'])
+    params = testutil.get_task_params(tasks[0])
+    self.assertEqual(str(source.key()), params['source_key'])
+    self.assertEqual('1970-01-01-00-00-00',
+                     params['last_polled'])
+
+  def test_create_new(self):
+    self.assertEqual(0, FakeSource.all().count())
+    self._test_create_new()
+
+  def test_create_new_already_exists(self):
+    FakeSource.new(self.handler).save()
+    FakeSource.key_name_counter -= 1
+    self._test_create_new()
+
+
+class PostTest(testutil.HandlerTest):
+
+  def setUp(self):
+    super(PostTest, self).setUp()
+    self.post = Post(key_name='tag:xyz', vars=json.dumps(POST_VARS))
+    appengine_config.USER_KEY_HANDLER_SECRET = 'my_secret'
+
+  def test_get_or_save(self):
+    self.assertEqual(0, Post.all().count())
+    self.assertEqual(0, len(self.taskqueue_stub.GetTasks('propagate')))
+
+    # new. should add a propagate task.
+    saved = self.post.get_or_save()
+    self.assertTrue(saved.is_saved())
+    self.assertEqual(self.post.key(), saved.key())
+
+    tasks = self.taskqueue_stub.GetTasks('propagate')
+    self.assertEqual(1, len(tasks))
+    self.assertEqual(str(self.post.key()),
+                     testutil.get_task_params(tasks[0])['post_key'])
+    self.assertEqual('/_ah/queue/propagate', tasks[0]['url'])
+
+    # existing. no new task.
+    same = saved.get_or_save()
+    self.assertEqual(1, len(tasks))
+
+  def test_envelope(self):
+    self.expect_urlfetch('https://facebook-webfinger.appspot.com/user_key'
+                         '?uri=acct:ryan@facebook.com&secret=my_secret',
+                         json.dumps(USER_KEY_JSON))
+    self.mox.ReplayAll()
+
+    envelope = self.post.envelope('acct:ryan@facebook.com')\
+        .replace('>', '>\n').replace('</', '\n</')
+    self.assert_multiline_equals(ENVELOPE_XML, envelope)
+
+  def test_send_slap(self):
+    pass
+
+
+class CommentTest(testutil.ModelsTest):
+
+  def test_get_or_save(self):
+    self.sources[0].save()
+
+    comment = self.comments[0]
+    self.assertEqual(0, Comment.all().count())
+    self.assertEqual(0, len(self.taskqueue_stub.GetTasks('propagate')))
+
+    # new. should add a propagate task.
+    saved = comment.get_or_save()
+    self.assertEqual(1, Comment.all().count())
+    self.assertTrue(saved.is_saved())
+    self.assertEqual(comment.key(), saved.key())
+    self.assertEqual(comment.source, saved.source)
+    self.assertEqual(comment.dest, saved.dest)
+
+    tasks = self.taskqueue_stub.GetTasks('propagate')
+    self.assertEqual(1, len(tasks))
+    self.assertEqual(str(comment.key()),
+                     testutil.get_task_params(tasks[0])['comment_key'])
+    self.assertEqual('/_ah/queue/propagate', tasks[0]['url'])
+
+    # existing. no new task.
+    same = saved.get_or_save()
+    self.assertEqual(saved.source.key(), same.source.key())
+    self.assertEqual(saved.dest.key(), same.dest.key())
+    self.assertEqual(1, len(tasks))
+
+    # # different source and dest
+    # # i don't do this assert any more, but i might come back to it later.
+    # diff = Comment(key_name=comment.key().name(),
+    #                source=self.sources[0], dest=self.dests[1])
+    # self.assertRaises(AssertionError, diff.get_or_save)
+    # diff = Comment(key_name=comment.key().name(),
+    #                source=self.sources[1], dest=self.dests[0])
+    # self.assertRaises(AssertionError, diff.get_or_save)
